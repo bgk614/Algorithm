67f4c89de3515bfa779426c70ead1ba91b010974
Bogyeong Kim <bogyeongkim614@gmail.com>
2023-04-11 23:24:55 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+def fibonacci(n):
+    a, b = 1, 1
+    if n == 0:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    for i in range(1, n):
+        a, b = b, a+b
+    return a
+
+print(fibonacci(n))
