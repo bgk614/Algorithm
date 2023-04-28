fd943ad7948124917e42dcb7542a74c0b8ad5727
Bogyeong Kim <bogyeongkim614@gmail.com>
2023-04-28 22:19:54 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(void){
+      int h, m;
+      std::cin >> h >> m;
+      if (m >= 45) std::cout << h <<" "<< (m-45);
+      else{
+            if (h == 0) std::cout << 23 <<" "<< (60+m-45);
+            else std::cout << (h-1) <<" "<< (60+m-45);
+      }
+      return 0;
+}
