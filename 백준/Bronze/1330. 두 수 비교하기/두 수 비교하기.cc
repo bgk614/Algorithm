9d7b478df73b3d5cffb4396fa9756fbacb3c9c4d
Bogyeong Kim <bogyeongkim614@gmail.com>
2023-04-28 21:46:39 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+      int a, b;
+      std::cin >> a >> b;
+      
+      if (a > b) std::cout<<">";
+      else if(a < b) std::cout<<"<";
+      else std::cout<<"==";
+
+      return 0;
+}
