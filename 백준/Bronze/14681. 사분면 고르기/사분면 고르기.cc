9fd9049c4b1db17db9c2ceef6d81cba24b511aa6
Bogyeong Kim <bogyeongkim614@gmail.com>
2023-04-28 22:10:41 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+int main(void){
+      int x, y;
+      std::cin >> x;
+      std::cin >> y;
+
+      if (x > 0){
+            if (y > 0) std::cout<<"1";
+                  else std::cout<<"4";
+      }
+      else{
+            if (y > 0) std::cout<<"2";
+                  else std::cout<<"3";
+      }
+      return 0;
+}
