3325dee7468fe937f9fc0add04944c9849cf5d6a
Bogyeong Kim <bogyeongkim614@gmail.com>
2023-05-20 02:47:54 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -6,10 +6,6 @@ int main(){
     while (1) {
         getline(cin, str);
         if(str[0] == '0') break;
-        else if(str.length() == 1){
-            cout<< "yes" <<'\n';
-            continue;
-        }
         for(int i=0; i<str.length(); i++) {
             if(str[i] != str[str.length() - 1 - i]) {
                 cout<< "no" <<'\n';
