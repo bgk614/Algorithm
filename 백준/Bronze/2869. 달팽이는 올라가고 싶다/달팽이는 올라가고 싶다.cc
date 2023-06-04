9580ff1fc25c0468be1e6263326242d90df818c2
Bogyeong Kim <bogyeongkim614@gmail.com>
2023-06-04 10:35:05 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -4,8 +4,6 @@ int main() {
     int a, b, v;
     cin>> a >> b >> v;
     
-    if(v == a) cout<< 1;
-    else if((v-a+b) <= a) cout<< 2;
-    else if((v-a) % (a-b) == 0) cout<<(v-a) / (a-b) + 1;
+    if((v-a) % (a-b) == 0) cout<<(v-a) / (a-b) + 1;
     else cout<<(v-a) / (a-b) + 2;
 }
