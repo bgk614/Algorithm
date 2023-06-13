8c4dfabd5d4841fa930a7a9e0071370acd962706
Bogyeong Kim <bogyeongkim614@gmail.com>
2023-06-13 21:39:00 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char ID[21];
+    cin>> ID;
+    cout<<":fan::fan::fan:"<<'\n';
+    cout<<":fan::"<<ID<<"::fan:"<<'\n';
+    cout<<":fan::fan::fan:";
+}
