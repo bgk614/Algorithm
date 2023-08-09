a026a1208b605c46c76f210b718d16c0f4364638
bgk <bogyeongkim614@gmail.com>
2023-08-09 21:08:59 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int N, K, A, B;
+
+    scanf("%d %d", &N, &K);
+    for(int i=0; i<N; i++) {
+        scanf("%d %d", &A, &B);
+    }
+    printf("비와이");
+
+    return 0;
+}
