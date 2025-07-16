022eed99a05528e1be251e84dffcdc4b685af7dc
Bogyeong Kim <bgk614@gmail.com>
2025-07-16 15:08:21 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 9472 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,12 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('', input => {
+  const [a, b] = input.trim().split(' ').map(Number);
+  console.log(`${a + b}`);
+  rl.close();
+});
