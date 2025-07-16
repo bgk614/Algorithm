29c347a30dd25ad746acea65bd21a4fa5d543645
Bogyeong Kim <bgk614@gmail.com>
2025-07-16 18:17:17 +0900

    [Bronze III] Title: 쌍의 합, Time: 92 ms, Memory: 9348 KB -BaekjoonHub

--- node.js/백준/Bronze/5217. 쌍의 합/쌍의 합.js
+++ node.js/백준/Bronze/5217. 쌍의 합/쌍의 합.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
-
 const t = Number(input[0]);
 
 for (let i = 1; i <= t; i++) {
     const n = Number(input[i]);
     let pairs = [];
-    
-    // i는 1부터 시작하고, i < n-i 조건으로 중복과 같은 수 방지
+
     for (let j = 1; j < n - j; j++) {
         pairs.push(`${j} ${n - j}`);
     }
